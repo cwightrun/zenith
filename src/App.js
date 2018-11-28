9b40dcc1787ff45bea46e3b5bdcf844275dbd247
Casey Wight <cwight@box.com>
2018-11-27 18:06:01 -0800

    Initial play work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// class Header extends Component {}
+function Header(props) {
+  return (
+    <header className="header">
+      <h3>Zenith Commander Counter.</h3>
+    </header>
+  )
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value,
+    };
+  }
+
+  handleSet(amt) {
+    this.setState({value: amt});
+  }
+
+  handleReset() {
+    this.setState({value: 0});
+  }
+
+  handleIncrement(dir) {
+    let newValue = this.state.value;
+    if (dir === 'negative') newValue = newValue - 1;
+    if (dir === 'positive') newValue = newValue + 1;
+
+    this.setState({value: newValue});
+  }
+
   render() {
     return (
+      <div className="counter-wrapper">
+        <div className="resets">
+          <button onClick={() => this.handleReset()}>Reset</button>
+          <button onClick={() => this.handleSet(20)}>Set to 20</button>
+          <button onClick={() => this.handleSet(40)}>Set to 40</button>
+        </div>
+        <div className="counter">
+          <button onClick={() => {this.handleIncrement('positive')}}>+</button>
+          <h1 className="current-value">{this.state.value}</h1>
+          <button onClick={() => {this.handleIncrement('negative')}}>-</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counters: [{
+        value: 0
+      }, {
+        value: 0
+      }, {
+        value: 0
+      }]
+    }
+  }
+
+  addCounter(type) {
+    var newCounters = this.state.counters.slice();
+    newCounters.push({value: 0});
+    this.setState({counters: newCounters});
+  }
+
+  render() {
+    let counters = this.state.counters;
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+
+        <main>
+
+          <p>Here's a thing.</p>
+
+          <button onClick={() => {this.addCounter('life')}}>Add Life Counter</button>
+          {counters.map((counter, i) =>
+            <Counter key={i} value={counter.value} />
+          )}
+
+          <style jsx>{`
+            p {
+              color: red;
+            }
+          `}</style>
+
+        </main>
       </div>
     );
   }
