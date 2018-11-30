388381ef88401aa2e0343e3fe5f772cd9503a5c9
Casey Wight <cwight@box.com>
2018-11-30 11:47:24 -0800

    Fixing add/remove func

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'mana-font';
-import Header from './Header/Header';
-import Counter from './Counter/Counter';
+import React, { Component } from "react";
+import uuid from "uuid/v4";
+import "./App.css";
+import "mana-font";
+import Header from "./Header/Header";
+import Counter from "./Counter/Counter";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counters: [
-        {id: 0, value: 40, icon: 0},
-        {id: 1, value: 20, icon: 1},
-        {id: 2, value: 20, icon: 2},
-        {id: 3, value: 20, icon: 3},
-      ],
-    }
-
-    console.log('this.state.counters', this.state.counters);
-
-    // Bindings
-    this.addCounter = this.addCounter.bind(this);
-    this.removeCounter = this.removeCounter.bind(this);
+        { id: uuid(), value: 40, icon: 0 },
+        { id: uuid(), value: 50, icon: 1 },
+        { id: uuid(), value: 60, icon: 2 },
+        { id: uuid(), value: 70, icon: 3 }
+      ]
+    };
   }
 
-  addCounter(type, id) {
-    let counters = [...this.state.counters]; // make a separate copy of the array
-    let length = counters.length;
+  addCounter = () => {
+    this.setState(prevState => ({
+      counters: [...prevState.counters, { id: uuid(), value: 0, icon: 0 }]
+    }));
+  };
 
-    counters.push({id: length, value: 0, icon: 0});
-    this.setState({counters: counters});
-  }
-
-  removeCounter(index) {
-    let counters = this.state.counters.filter((counter, counterIndex) => {
-      return counterIndex !== index
-    })
-
-    this.setState({ counters })
-  }
+  removeCounter = (id) => {
+    this.setState(prevState => ({
+      counters: prevState.counters.filter(counter => counter.id !== id)
+    }));
+  };
 
   render() {
-    let counters = this.state.counters;
+    console.log(this.state);
     return (
       <div className="App">
         <Header />
         <main>
           <div className="manage-counters">
-            <button onClick={() => {this.addCounter('add', counters.length)}}>Add Counter</button>
+            <button onClick={() => {this.addCounter()}}>Add Counter</button>
           </div>
           <div className="all-counters">
-            {counters.map((counter, i) =>
-              <Counter removeCounter={this.removeCounter} id={i} key={i} value={this.state.counters[i].value} icon={this.state.counters[i].icon} />
-            )}
+            {this.state.counters.map((counter, i) => (
+              <Counter
+                removeCounter={this.removeCounter}
+                counter={counter}
+                key={counter.id}
+              />
+            ))}
           </div>
         </main>
       </div>
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -5,63 +5,108 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: this.props.id,
-      value: this.props.value,
-      icon: this.props.icon
+      value: this.props.counter.value,
+      icon: this.props.counter.icon
     };
   }
 
-  handleChange(dir, amt = 1) {
-    let newValue = this.state.value;
-    if (dir === 'negative') newValue = newValue - amt;
-    if (dir === 'positive') newValue = newValue + amt;
-    if (dir === 'reset') newValue = amt;
-    this.setState({value: newValue});
-    console.log('counter state', this.state);
-  }
+  handleChange = (dir, amt = 1) => {
+    let value = this.state.value;
+    if (dir === "negative") value = value - amt;
+    if (dir === "positive") value = value + amt;
+    if (dir === "reset") value = amt;
+    this.setState({ value });
+  };
 
-  changeIcon() {
+  changeIcon = () => {
     if (this.state.icon <= 5) {
-      this.setState({icon: this.state.icon + 1});
+      this.setState({ icon: this.state.icon + 1 });
     } else {
-      this.setState({icon: 0});
+      this.setState({ icon: 0 });
     }
-  }
+  };
 
   render() {
     return (
-      <div className="counter-wrapper" style={{backgroundColor: mtgThemes[this.state.icon]['bg']}}>
+      <div
+        className="counter-wrapper"
+        style={{ backgroundColor: mtgThemes[this.state.icon]["bg"] }}
+      >
         <div className="counter">
-
           <div className="increment-column col-pos button-group has-3-buttons">
-            <button className="value-1" onClick={() => {this.handleChange('positive', 1)}}>+1</button>
-            <button className="value-5" onClick={() => {this.handleChange('positive', 5)}}>+5</button>
-            <button className="value-10" onClick={() => {this.handleChange('positive', 10)}}>+10</button>
+            <button
+              className="value-1"
+              onClick={() => {
+                this.handleChange("positive", 1);
+              }}
+            >
+              +1
+            </button>
+            <button
+              className="value-5"
+              onClick={() => {
+                this.handleChange("positive", 5);
+              }}
+            >
+              +5
+            </button>
+            <button
+              className="value-10"
+              onClick={() => {
+                this.handleChange("positive", 10);
+              }}
+            >
+              +10
+            </button>
           </div>
 
           <div className="value-wrapper">
-            <button onClick={() => this.changeIcon()} className="counter-icon" style={{backgroundColor: mtgThemes[this.state.icon]['fg']}}>
-              <i className={'ms ms-' + mtgThemes[this.state.icon]['icon']}></i>
+            <button
+              onClick={() => this.changeIcon()}
+              className="counter-icon"
+              style={{ backgroundColor: mtgThemes[this.state.icon]["fg"] }}
+            >
+              <i className={"ms ms-" + mtgThemes[this.state.icon]["icon"]} />
             </button>
             <h4 className="current-value">{this.state.value}</h4>
           </div>
 
           <div className="increment-column col-neg button-group has-3-buttons">
-            <button className="value-10" onClick={() => {this.handleChange('negative', 10)}}>-10</button>
-            <button className="value-5" onClick={() => {this.handleChange('negative', 5)}}>-5</button>
-            <button className="value-1" onClick={() => {this.handleChange('negative', 1)}}>-1</button>
+            <button
+              className="value-10"
+              onClick={() => {
+                this.handleChange("negative", 10);
+              }}
+            >
+              -10
+            </button>
+            <button
+              className="value-5"
+              onClick={() => {
+                this.handleChange("negative", 5);
+              }}
+            >
+              -5
+            </button>
+            <button
+              className="value-1"
+              onClick={() => {
+                this.handleChange("negative", 1);
+              }}
+            >
+              -1
+            </button>
           </div>
-
         </div>
 
-        <button onClick={() => this.props.removeCounter(this.props.id)}>Delete</button>
+        <button onClick={() => this.props.removeCounter(this.props.counter.id)}>Delete</button>
 
         <div className="resets button-group has-5-buttons">
-          <button onClick={() => this.handleChange('reset', 0)}>0</button>
-          <button onClick={() => this.handleChange('reset', 20)}>20</button>
-          <button onClick={() => this.handleChange('reset', 40)}>40</button>
-          <button onClick={() => this.handleChange('reset', 60)}>60</button>
-          <button onClick={() => this.handleChange('reset', 80)}>80</button>
+          <button onClick={() => this.handleChange("reset", 0)}>0</button>
+          <button onClick={() => this.handleChange("reset", 20)}>20</button>
+          <button onClick={() => this.handleChange("reset", 40)}>40</button>
+          <button onClick={() => this.handleChange("reset", 60)}>60</button>
+          <button onClick={() => this.handleChange("reset", 80)}>80</button>
         </div>
       </div>
     );
